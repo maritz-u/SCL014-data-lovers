f5c06abe4729edd357248d7e301b6c7eb6f4bf76
Fabiane Santos <fanutri1@gmail.com>
2020-07-04 12:52:26 -0400

    se imprime en pantalla los pokemones con js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import data from "./data/pokemon/pokemon.js";
 
-// console.log(data);
+console.log(data);
 
 const move = (close, open) => {
   document.getElementById(close).style.display = "none";
@@ -14,11 +14,30 @@ btn.addEventListener("click", () => {
 
 const btnForo = document.getElementById("btnForo");
 btnForo.addEventListener("click", () => {
-  move("container", "showResult");
+  move("container", "foro");
 });
-
 
 const btnVolver = document.getElementById("btnCerrar");
 btnVolver.addEventListener("click", () => {
   move("showResult", "container");
+});
+
+let pokemonList = data.pokemon;
+// console.log(pokemonList[3].name);
+
+let poket = document.getElementById("pokemones");
+
+pokemonList.forEach(elem => {
+  // pokemom.innerHTML += 'Nombre: '+ elem.name;
+  poket.innerHTML += `
+<br>
+<h2> ${elem.name.toUpperCase()}</h2>
+<h2> <img src=" ${elem.img}"> </h2>
+<p>${elem.about} </p> 
+<p> Numero: ${elem.num}</p>
+<p> Tipo: ${elem.type} </p>
+<p> Fortalezas: ${elem.resistant} </p>
+<p> Debilidades: ${elem.weaknesses} </p>
+<br>
+`;
 });
