07f35c7694fe2e94cf4a40d1f666c080e26b5ca2
Maritzu Zúñiga <maritzudelosangeles@gmail.com>
2020-07-06 23:13:24 -0400

    corrige merch y añade imagenes centradas

--- src/main.js
+++ src/main.js
@@ -14,21 +14,10 @@ btn.addEventListener("click", () => {
   move("container", "showResult");
 });
 
-// función para botón que lleva a la un link externo para Foros
-const btnForo = document.getElementById("btnForo");
-btnForo.addEventListener("click", () => {
-  move("container", "foro");
-});
-
 // función para botón que cierra la segunda pantalla y vuelve al pantalla de inicio
 const btnClose = document.getElementById("btnCerrar");
 btnClose.addEventListener("click", () => {
   move("showResult", "container");
-});
-
-const btnCloseForo = document.getElementById("btnCerrarForo");
-btnCloseForo.addEventListener("click", () => {
-  move("foro", "container");
 });
 
 // variable que guarda la data pokemon
@@ -39,14 +28,14 @@ let pokemonList = data.pokemon;
    metodo forEach, donde tenemos acceso al array de la propiedad pokemon, que por su vez, es compuesto por varios objetos.
   Html dinamico, donde las comillas invertidas  delimitan las cadenas y con el ${} obtenemos el valor de la propiedad solicitada.
   Se agrega clase 'card' para estilo con flexbox.
-  Se agrega clase 'desaparece' para dejar oculta algunas informaciones de los pokemones */
+  Se agrega clase 'desaparece' para dejar oculta algunas informaciones de los pokemones*/
 
 let pokeCard = document.getElementById("pokemones");
 pokemonList.forEach(elem => {
   // pokemom.innerHTML += 'Nombre: '+ elem.name;
   pokeCard.innerHTML += `
   <div id="card" class=" card">
-    <div id="" class="frente">
+    <div id="frente" class="frente">
       <p> ${elem.name.toUpperCase()}</p>
        <p> <img src=" ${elem.img}"> </p>
     </div>
@@ -62,4 +51,29 @@ pokemonList.forEach(elem => {
 
 `;
 });
+
+
+
+
+/* función que captura el id'card', y cuan do se haceclick en la carta pokemon, nos muestra las informaciones ocultas*/
+// let btnChange = document.getElementById("card");
+// btnChange.addEventListener("click", () => {
+//   // pokemonList.forEach(elem => {
+//   // if (elem){
+//   move("uno", "a");
+//   move("uno", "b");
+//   move("uno", "c");
+//   move("uno", "d");
+//   move("uno", "e");
+
+//   // }
+//   // }
+// });
+
+
+// let bla = document.getElementsByClassName('desaparece');
+// btnChange.addEventListener("click", () => {
+// bla.forEach(elem=>{
+// bla.classList.toggle('desaparece');
+// })
 
