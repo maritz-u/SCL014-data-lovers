b4da2eba1cec7ef54a3954f41cb3ea0bd7602d7a
Maritzu Zúñiga <maritzudelosangeles@gmail.com>
2020-07-09 20:06:23 -0400

    Parte logica del proyecto

--- src/data.js
+++ src/data.js
@@ -1,8 +1,35 @@
-// estas funciones son de ejemplo
+const filterByGeneration = (data, valor) => {
+  console.log(data, valor);
+  const filterPokemonByGeneration = data.filter(
+    pokemon => pokemon.generation.name === valor
+  );
+  console.log(filterPokemonByGeneration);
 
-export const example = () => 'example';
+  return filterPokemonByGeneration;
+};
 
+const filterByType = (data, valor) => {
+  console.log(data, valor);
+  const filterPokemonByType = data.filter(
+    pokemon => pokemon.type.indexOf(valor.toLowerCase()) > -1
+  );
+  console.log(filterPokemonByType);
 
+  return filterPokemonByType;
+};
 
-export const anotherExample = () => 'OMG';
+const orderByName = (data, valor) => {
+  console.log(data, valor);
 
+  if (valor === "A-Z") {
+    data.sort(
+      (a, b) => a.name.localeCompare(b.name)
+    );
+  } else {
+    data.sort(
+      (a, b) => b.name.localeCompare(a.name)
+    );
+  }
+};
+
+export { filterByGeneration, filterByType, orderByName };
